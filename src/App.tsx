aa0d360ec922281a6d584b0d5d3fb5c6d1cc6402
Youzege <Youzegehq@gmail.com>
2023-06-03 23:40:59 +0800

    chore: 配置基本env环境配置

--- src/App.tsx
+++ src/App.tsx
@@ -1,6 +1,8 @@
 import { TestComponent } from '@/components';
 
 const App = () => {
+  console.log(import.meta.env);
+
   return (
     <>
       <h1 className="tw-text-emerald-400">Vite + React</h1>
--- src/vite-env.d.ts
+++ src/vite-env.d.ts
@@ -1,1 +1,11 @@
 /// <reference types="vite/client" />
+
+interface ImportMetaEnv {
+  readonly VITE_APP_NAME: string;
+  readonly VITE_MODE: string;
+  // 更多环境变量...
+}
+
+interface ImportMeta {
+  readonly env: ImportMetaEnv;
+}
