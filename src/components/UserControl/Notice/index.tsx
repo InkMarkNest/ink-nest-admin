b835155ff7ce44085e3d6b662627aba57d0d53a8
Lizh606 <915194732@qq.com>
2023-06-29 17:39:19 +0800

    feat(base): 通知组件接入antdStyle样式

--- src/components/UserControl/Notice/index.tsx
+++ src/components/UserControl/Notice/index.tsx
@@ -2,9 +2,32 @@ import { FC, useState } from 'react';
 
 import { Button } from 'antd';
 
+import { createStyles } from 'antd-style';
+
 import { ReactComponent as NoticeSvg } from '@/assets/svg/common-icon/notice.svg';
 
 import { SvgIcon } from '../../Icon';
+
+const useStyles = createStyles(({ token, css }) => ({
+  panel: css`
+    background: ${token.colorPrimaryBg};
+    border: 1px solid ${token.colorPrimaryBorder};
+    color: ${token.colorPrimaryText};
+  `,
+  bottomLine: css`
+    border-bottom: 1px solid ${token.colorPrimaryBorder};
+  `,
+  title: css`
+    &:hover {
+      background: ${token.colorBgTextHover};
+      color: ${token.colorPrimaryTextHover};
+    }
+    &:focus {
+      background: ${token.colorBgTextActive};
+      color: ${token.colorPrimaryTextActive};
+    }
+  `,
+}));
 
 const ContentPanel: FC = () => {
   const contentList = {
@@ -13,29 +36,33 @@ const ContentPanel: FC = () => {
     Todo: [],
   };
   const typeList = Object.keys(contentList);
+  const { styles, cx } = useStyles();
   return (
     <div className="tw-flex tw-flex-col">
-      <div className="tw-flex tw-gap-4">
+      <div className={cx('tw-flex tw-gap-4 tw-p-2', styles.bottomLine)}>
         {typeList.map((key: string, index: number) => {
           return (
             <div
-              className="tw-cursor-pointer tw-px-4 tw-py-2 tw-font-standard  hover:tw-rounded-3xl hover:tw-bg-[#eee] focus:tw-rounded-3xl focus:tw-bg-[#eee] focus:tw-text-blue-600"
+              className={cx(
+                'tw-cursor-pointer tw-px-4 tw-py-2 tw-font-standard  hover:tw-rounded-3xl  focus:tw-rounded-3xl  ',
+                styles.title,
+              )}
               tabIndex={index}
               key={key}
             >
               {key}
             </div>
-            // <Button type="primary" ghost>
-            //   {key}
-            // </Button>
           );
         })}
       </div>
+      <div className={cx('tw-p-2', styles.bottomLine)}>todo:消息列表</div>
+      <div className={cx('tw-p-2')}>todo:底部按钮</div>
     </div>
   );
 };
 const Notice: FC = () => {
   const [isShowList, setShoState] = useState(false);
+  const { styles, cx } = useStyles();
   return (
     <div className="tw-relative">
       <Button
@@ -47,7 +74,9 @@ const Notice: FC = () => {
         icon={<SvgIcon element={NoticeSvg} size="large" />}
       />
       {isShowList && (
-        <div className="tw-absolute tw-right-0 tw-z-10  tw-rounded-lg tw-bg-white tw-p-4 tw-shadow-lg">
+        <div
+          className={cx('tw-absolute tw-right-0 tw-z-10  tw-rounded-lg tw-shadow-lg', styles.panel)}
+        >
           <ContentPanel />
         </div>
       )}
